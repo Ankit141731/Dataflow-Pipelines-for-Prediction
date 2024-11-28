b98217694dfad0db565c42c5fef4d7b2723c12a5
Ankit141731 <ac7265036@gmail.com>
2024-11-28 21:41:02 +0530

    ---

---
+++ src/logger/__init__.py
@@ -0,0 +1,21 @@
+import os , sys
+import logging
+from datetime import datetime
+
+LOG_DIR = "logs"
+LOG_DIR = os.path.join(os.getcwd() , LOG_DIR)
+
+os.makedirs(LOG_DIR , exist_ok = True)
+
+CURRENT_TIME_STAMP = f'{datetime.now().strftime("%d-%m-%y-%H-%M-%S")}'
+
+LOG_FILE_NAME = f"log_{CURRENT_TIME_STAMP}.log"
+
+LOG_FILE_PATH = os.path.join(LOG_DIR , LOG_FILE_NAME)
+
+logging.basicConfig(
+    filename = LOG_FILE_PATH ,
+    filemode = "w" ,
+    format = '%(asctime)s-%(filename)s-%(level)s-%(message)s',
+    level = logging.INFO,
+)
